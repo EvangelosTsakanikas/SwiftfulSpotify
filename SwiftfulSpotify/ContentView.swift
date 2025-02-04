979f530a095014ea0d585f0b9114df0dab307220
Evangelos Tsakanikas <evangelostsakanikas@gmail.com>
2025-02-04 18:58:19 +0100

    Initial Commit

---
+++ SwiftfulSpotify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftfulSpotify
+//
+//  Created by user274186 on 2/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftfulSpotify/SwiftfulSpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSpotifyApp.swift
+//  SwiftfulSpotify
+//
+//  Created by user274186 on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
